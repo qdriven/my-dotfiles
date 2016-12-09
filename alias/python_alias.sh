4d2ccc3c147566a8c062cfa3056f6e0937d97732
evenhumble <patrickwuke@163.com>
2016-12-09 12:48:52 +0800

    add source in python_alias

--- alias/python_alias.sh
+++ alias/python_alias.sh
@@ -5,3 +5,5 @@ echo "setting up in $RC_FILE for PYTHON"
 echo '# python3 setting' >> $RC_FILE
 echo 'alias pvm="python3 -m venv "'>> $RC_FILE
 echo 'alias pip="pip --trusted-host pypi.douban.com"' >>$RC_FILE
+echo 'source ~/.zshrc'
+source ~/.zshrc
