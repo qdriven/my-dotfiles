9ec9ebbe33ccd9f0832afc8099611d48188e4d4e
Patrick <patrickwuke@163.com>
2022-01-06 13:42:56 +0800

    add ruby install scripts

--- git/setup.sh
+++ git/setup.sh
@@ -2,3 +2,8 @@
 git config --global http.sslVerify "false"
 git config --global core.compression 0
 git config --global http.postBuffer 1048576000
+
+
+
+##
+ssh-keygen -t rsa -C "your_email@example.com"
---
+++ mac/sdk/ruby_install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+curl -L get.rvm.io | bash -s stable
+rvm list known
+rvm install 3.0.0
+rvm list
+## set default ruby version
+rvm 3.0.0 --default
+# run shell in ruby
+rbenv shell 3.0.0
+## set default ruby version
+rvm 3.0.0 --default
+
+## set ruby source
+gem source -r https://rubygems.org/
+gem source -a https://gems.ruby-china.com
+gem sources -l
+
+# sudo gem update --system
+sudo gem update -n /usr/local/bin —system
+
+# install cocoapods
+brew install cocoapods
+pod --version
+brew update
+brew upgrade cocoapods
+
+sudo gem install -n /usr/local/bin cocoapods
+sudo gem install cocoapods -n /usr/local/bin
+pod setup
+pod repo add master https://github.com/CocoaPods/Specs.git
+pod repo list
+
+gem list
+# uninstall
+# sudo gem uninstall cocoapods
+rm -rf ~/Library/Application\Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcpluginrm -rf ~/Library/Application\Support/Alcatraz
+
+
